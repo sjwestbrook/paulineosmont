dc036b35eb5ad1790160dfa081294cf845a76ed0
sjwestbrook <sawest4291@gmail.com>
2016-02-03 09:28:42 -0700

    misc

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-var myAppModule = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ui.router']);
+
+myApp.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/home');
+  
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: 'js/partials/home.html'
+    })
+    .state('about', {
+      url: '/about',
+      templateUrl: 'js/partials/about.html'
+    })
+    .state('contact', {
+      url: '/contact',
+      templateUrl: 'js/partials/contact.html'
+    });
+})
